83f4329da55c88f4a826c797027b59a32afc1795
Impulse2020 <irishht@gmail.com>
2020-05-12 15:59:10 -0400

    Completes task 4 and MVP

--- src/App.js
+++ src/App.js
@@ -7,6 +7,60 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [home, setHome] = useState(0);
   const [away, setAway] = useState(0);
+
+
+const score = event =>{
+  console.log(event.target.className)
+ 
+    if(event.target.className === "homeButtons__touchdown"){
+
+      setHome(home + 7);
+
+
+
+    }
+    else if(event.target.className === "homeButtons__fieldGoal" ){
+
+      setHome(home + 3)
+
+    }
+
+    else if(event.target.className === "awayButtons__touchdown" ){
+
+      setAway(away + 7)
+
+    }
+    else if(event.target.className === "awayButtons__fieldGoal" ){
+
+
+      setAway(away + 3)
+
+    }
+  
+      
+     // 
+
+
+   //   
+      
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -29,12 +83,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={score} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={score}className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick= {score} className="awayButtons__touchdown" >Away Touchdown</button>
+          <button onClick= {score} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
